ad8897024d57bd418d5d0e26fa429059600c3f35
HenningJW <HenningJW@users.noreply.github.com>
2018-05-09 21:41:56 +0200

    WIP: Use mint Kernel as base part 8
    just another checkpoint see commits before for a better description

--- arch/arm/kernel/arch_timer.c
+++ arch/arm/kernel/arch_timer.c
@@ -15,6 +15,7 @@
 #include <linux/device.h>
 #include <linux/smp.h>
 #include <linux/cpu.h>
+#include <linux/cpu_pm.h>
 #include <linux/jiffies.h>
 #include <linux/clockchips.h>
 #include <linux/interrupt.h>
@@ -293,7 +294,7 @@ static int arch_timer_available(void)
 	return 0;
 }
 
-static inline cycle_t counter_get_cntpct_mem(void)
+static inline cycle_t notrace counter_get_cntpct_mem(void)
 {
 	u32 cvall, cvalh, thigh;
 
@@ -306,7 +307,7 @@ static inline cycle_t counter_get_cntpct_mem(void)
 	return ((cycle_t) cvalh << 32) | cvall;
 }
 
-static inline cycle_t counter_get_cntpct_cp15(void)
+static inline cycle_t notrace counter_get_cntpct_cp15(void)
 {
 	u32 cvall, cvalh;
 
@@ -314,7 +315,7 @@ static inline cycle_t counter_get_cntpct_cp15(void)
 	return ((cycle_t) cvalh << 32) | cvall;
 }
 
-static inline cycle_t counter_get_cntvct_mem(void)
+static inline cycle_t notrace counter_get_cntvct_mem(void)
 {
 	u32 cvall, cvalh, thigh;
 
@@ -327,7 +328,7 @@ static inline cycle_t counter_get_cntvct_mem(void)
 	return ((cycle_t) cvalh << 32) | cvall;
 }
 
-static inline cycle_t counter_get_cntvct_cp15(void)
+static inline cycle_t notrace counter_get_cntvct_cp15(void)
 {
 	u32 cvall, cvalh;
 
@@ -359,7 +360,7 @@ static struct clocksource clocksource_counter = {
 	.rating	= 400,
 	.read	= arch_counter_read,
 	.mask	= CLOCKSOURCE_MASK(56),
-	.flags	= CLOCK_SOURCE_IS_CONTINUOUS,
+	.flags	= CLOCK_SOURCE_IS_CONTINUOUS | CLOCK_SOURCE_SUSPEND_NONSTOP,
 };
 
 static u32 arch_counter_get_cntvct32(void)
@@ -410,6 +411,33 @@ static void __init arch_timer_counter_init(void)
 	register_current_timer_delay(&arch_delay_timer);
 }
 
+#ifdef CONFIG_CPU_PM
+static unsigned int saved_cntkctl;
+static int arch_timer_cpu_pm_notify(struct notifier_block *self,
+				unsigned long action, void *hcpu)
+{
+	if (action == CPU_PM_ENTER)
+		saved_cntkctl = arch_timer_get_cntkctl();
+	else if (action == CPU_PM_ENTER_FAILED || action == CPU_PM_EXIT)
+		arch_timer_set_cntkctl(saved_cntkctl);
+	return NOTIFY_OK;
+}
+
+static struct notifier_block arch_timer_cpu_pm_notifier = {
+	.notifier_call = arch_timer_cpu_pm_notify,
+};
+
+static int __init arch_timer_cpu_pm_init(void)
+{
+	return cpu_pm_register_notifier(&arch_timer_cpu_pm_notifier);
+}
+#else
+static int __init arch_timer_cpu_pm_init(void)
+{
+	return 0;
+}
+#endif
+
 static int __init arch_timer_common_register(void)
 {
 	int err;
@@ -445,6 +473,10 @@ static int __init arch_timer_common_register(void)
 		}
 	}
 
+	err = arch_timer_cpu_pm_init();
+	if (err)
+		goto out_free_irq;
+
 	err = local_timer_register(&arch_timer_ops);
 	if (err) {
 		/*
@@ -458,10 +490,12 @@ static int __init arch_timer_common_register(void)
 	}
 
 	if (err)
-		goto out_free_irq;
+		goto out_unreg_notify;
 
 	return 0;
 
+out_unreg_notify:
+	cpu_pm_unregister_notifier(&arch_timer_cpu_pm_notifier);
 out_free_irq:
 	free_percpu_irq(arch_timer_ppi, arch_timer_evt);
 	if (arch_timer_ppi2)
@@ -495,8 +529,8 @@ static int __init arch_timer_mem_register(void)
 	clockevents_config_and_register(clk, arch_timer_rate,
 					0xf, 0x7fffffff);
 
-	err = request_irq(arch_timer_spi, arch_timer_handler_mem, 0,
-		"arch_timer", clk);
+	err = request_irq(arch_timer_spi, arch_timer_handler_mem,
+			IRQF_TIMER, "arch_timer", clk);
 
 	return err;
 }
